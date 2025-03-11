88668bcfd75444f185666553943d1c0555b17dbb
Filip <fhagno@yahoo.com>
2025-03-11 12:42:07 +0100

    Init

---
+++ src/test/java/pl/filiphagno/springreactivemongo/SpringReactiveMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.filiphagno.springreactivemongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactiveMongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
