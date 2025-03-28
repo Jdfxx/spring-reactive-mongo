95876577ac7a163735ab7a631b3d07c0c907b557
Filip <fhagno@yahoo.com>
2025-03-28 12:04:39 +0100

    Add actuator and its security config

--- src/main/java/pl/filiphagno/springreactivemongo/config/SecurityConfig.java
+++ src/main/java/pl/filiphagno/springreactivemongo/config/SecurityConfig.java
@@ -4,12 +4,27 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        return http
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers("/actuator/health/**").permitAll()
+                        .pathMatchers("/actuator/info").permitAll()
+                        .anyExchange().authenticated()  // Secure all other endpoints
+                )
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)  // Disable CSRF for simplicity (adjust for your needs)
+                .formLogin(ServerHttpSecurity.FormLoginSpec::disable)  // Disable form login
+                .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)  // Disable basic auth
+                .build();
+    }
 
     @Bean
     SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http){
